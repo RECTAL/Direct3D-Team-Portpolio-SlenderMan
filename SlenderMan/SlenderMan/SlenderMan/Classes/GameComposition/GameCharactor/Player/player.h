a86d00e63fcc406407e241ab1478178f0edc9f0e
RECTAL <33717935+RECTAL@users.noreply.github.com>
2019-02-15 21:25:18 +0900

    병합커밋

--- SlenderMan/SlenderMan/SlenderMan/Classes/GameComposition/GameCharactor/Player/player.h
+++ SlenderMan/SlenderMan/SlenderMan/Classes/GameComposition/GameCharactor/Player/player.h
@@ -6,9 +6,59 @@
 
 class CCameraObject;
 class SpotLightObject;
-
+class CMainPlayScene;
+class CSkinnedObject;
+class CStage;
 class player : public CCharactor
 {
+public:
+	player();
+	virtual ~player(void);
+public:
+	virtual void init(void) override;
+	virtual void update(void) override;
+
+	virtual void preDraw(void) override;
+	virtual void doDraw(void) override;
+	virtual void postDraw(void) override;
+
+public:		//getter,setter
+	/**********************************************/
+	//getter
+	/**********************************************/
+	CCameraObject* getCamera(void) { return cameraObj; }
+	CSpotLightObject* getLightObj(void) { return lightObj; }
+	int &getPlayerState(void) { return playerState; }
 
 
+
+	/**********************************************/
+	//setter
+	/**********************************************/
+	void	setStage(CStage* a_pStage) { m_pStage = a_pStage; }
+	void mainSceneAddress(CMainPlayScene* address) { mainScene = address; }
+private:
+	void mouseSenterPos();
+	CSkinnedObject* createPlayer();
+private:
+	void settingCamera();
+	void settingLight();
+
+
+	void adjustCollisionArea();
+	
+
+private:
+	CCameraObject* cameraObj = nullptr;
+	CSpotLightObject* lightObj = nullptr;
+	CMainPlayScene* mainScene = nullptr;
+	CSkinnedObject*		m_pSkinnedObj = nullptr;
+	CStage*		m_pStage = nullptr;
+
+	int playerState = (int)EPlayerState::NONE;
+	bool isEsc = false;
+
+	fPOINT		m_fTopLeft;
+	fPOINT		m_fBottomRight;
+	float		m_fCheckRange = 20.0f;
 };
